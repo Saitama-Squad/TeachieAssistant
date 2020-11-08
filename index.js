b32cf111c7f2b520093dfd2910588c107a87dd90
Lokesh N <lokesh19055@cse.ssn.edu.in>
2020-11-08 15:51:44 +0530

    make it better

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 require("./models/UserInfo");
 require("./Services/PassportOauth");
 const keys = require("./config/keys");
+const path = require("path");
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
@@ -37,6 +38,13 @@ app.use(passport.session());
 
 app.set("view engine", "ejs");
 require("./Routes/authRoutes.js")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("App listening on port " + port));
