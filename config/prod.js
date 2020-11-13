15435daed5a1ce3fccb2dadd8c3b5c70df8d1e9b
Lokesh N <lokesh19055@cse.ssn.edu.in>
2020-11-13 17:41:11 +0530

    added

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
 };
